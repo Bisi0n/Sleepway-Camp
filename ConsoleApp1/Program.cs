d552e4aea199b28c4a9e092a73a1646179efe303
Bisi0n <110070415+Bisi0n@users.noreply.github.com>
2023-01-02 15:46:18 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int option = Utilities.ShowMenu("Please choose one of the following: ", new[]
+            {
+                ""
+            });
+        }
+    }
+}
---
+++ EntityFramework/Context/CampsContext.cs
@@ -0,0 +1,35 @@
+﻿using Sleepway.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sleepway.Context
+{
+    public class CampsContext : DbContext
+    {
+        public CampsContext() 
+        {
+            
+        }
+
+        public CampsContext(DbContextOptions<CampsContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Cabin> Cabins { get; set; }
+        public DbSet<Camper> Campers { get; set; }
+        public DbSet<Counselor> Counselors { get; set; }
+        public DbSet<NextOfKin> NextOfKins { get; set; }
+
+        private readonly string connection = @"Server=DESKTOP-GHKGJ8K\SQLEXPRESS;Database=Sleepway;Trusted_Connection=True;Integrated Security=true;TrustServerCertificate=true;"; 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connection);
+        }
+    }
+}
